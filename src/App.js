6810388cefb0b872ee7173094511e8df52e661a2
Casey The Great <casey@caseypitman.com>
2020-10-08 15:56:47 -0500

    Basic jsx mock-up

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import React, {useState} from 'react';
 
 function App() {
 
-  const [userChoice, setUserChoice] = useState(null);
-  const [opponnentChoice, setOpponentChoice] = useState(null);
+  const [userChoice, setUserChoice] = useState('');
+  const [opponnentChoice, setOpponentChoice] = useState('');
 
 
+  // on click. choices disappear. There is a counter 'three two one shoot'
+  //Your choice shows up. Then a second later your opponent's choice shows up. one second delay -  Winner turns blue, loser turns red. result text appears.
 
   return (
     <div className='App'>
@@ -17,6 +19,12 @@ function App() {
         <i class="far fa-hand-paper"></i>
         <i class="far fa-hand-scissors"></i>
       </div>
+      <div className = 'result'>
+        <h1 className = 'win-status'>You win!</h1>
+        <h2 className = 'win-explain'>You chose rock. Your opponent chose scissors. Rock smashes scissors.</h2>
+        <h3>Play Again.</h3>
+      </div>
+      
     </div>
   );
 }
