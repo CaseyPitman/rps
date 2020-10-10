46656e51407488a196dda71c2832127409813428
Casey The Great <casey@caseypitman.com>
2020-10-10 09:41:45 -0500

    Trying to figure out how to keep timer from re-rendering infinitely.

--- src/Countdown.js
+++ src/Countdown.js
@@ -5,34 +5,32 @@ import React, {useState, useEffect} from 'react';
 const Countdown = (props) => {
    
 
-   const [countdownText, setCountdownText] = useState(null)
+   const [countdownText, setCountdownText] = useState('Ready?')
 
-   const countdownElements = ['Three', 'Two', 'One', 'Shoot!'];
-
-
+   const countdownElements = ['Three...', 'Two...', 'One...', 'Shoot!'];
 
 
-   useEffect(()=>{
-      let i = 3;
-      setInterval (() => {
-         if(i>0){
-         console.log(i);
-         i--;
-         } else {
-            clearInterval();
-            console.log('shoot!')
-            //then call to playArea to advance to results.
-         }
+
+   let i = 0;
+   let countdownTimer = setInterval (() => {
+      if(i<4){
+
+         //This keeps rerendering infitiely. Each rerender runs it again?
+         // setCountdownText(countdownElements[i])
+         console.log(countdownText)
+      
+         i++;
+      } else {
+         clearInterval(countdownTimer);
+         console.log('end')
+         //then call to playArea to advance to results.
+        }
       }, 1000)
-
-     
-
-   })
 
 
    return (
       <div className = "countdown" >
-         <h1 className = "countdown-text">three</h1>
+         <h1 className = "countdown-text">test</h1>
       </div>
    )
 }
