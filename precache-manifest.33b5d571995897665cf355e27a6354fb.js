fb098f51c235c4e7d23136ebf0ff70603186dc26
Casey The Great <casey@caseypitman.com>
2020-10-12 13:48:58 -0500

    Updates

---
+++ precache-manifest.33b5d571995897665cf355e27a6354fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb9f69423e680cf7612882614c1afd89",
+    "url": "/rps/index.html"
+  },
+  {
+    "revision": "c2d64787210f311ae979",
+    "url": "/rps/static/css/main.50c0a1bc.chunk.css"
+  },
+  {
+    "revision": "256bce2855d288ac395f",
+    "url": "/rps/static/js/2.b704d7ef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rps/static/js/2.b704d7ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2d64787210f311ae979",
+    "url": "/rps/static/js/main.f850c644.chunk.js"
+  },
+  {
+    "revision": "8f49c4e7775678442159",
+    "url": "/rps/static/js/runtime-main.2df09ef6.js"
+  },
+  {
+    "revision": "41da84aa56c28b4a0f555d217cec20cc",
+    "url": "/rps/static/media/chalkboard.41da84aa.jpg"
+  }
+]);
